db681163eac1e4890b04e1168ba1947bee3692d3
root <root@LOPESNOTEBOOK.localdomain>
2022-11-08 19:00:53 -0300

    Desafio nginx como proxy para nodejs

---
+++ desafioNginxNode/node/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql')
+
+const connection = mysql.createConnection(config)
+
+const insert = `INSERT INTO people(name) values ('anizair')`
+connection.query(insert)
+connection.end()
+
+const qry = `Select * from people`
+
+app.get('/', (req, resp) =>{
+
+    let connection = mysql.createConnection(config);
+
+    let sql = `SELECT * FROM people`;
+    connection.query(sql, (error, results, fields) => {
+      if (error) {
+        return console.error(error.message);
+      }
+      resp.send('<h1>Full Cycle Rocks!</h1> <br/> <p style="font-size: 25px;"style="font-size: 25px;"> ' + results[0].id + ' - ' + results[0].name + '</p>' )
+    });
+    
+    connection.end();
+});
+ 
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
